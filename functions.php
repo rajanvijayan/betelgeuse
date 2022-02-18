e9020ae442a346509c003a9f3c31a1b86714e2db
Rajan Vijayan <rajanit2000@gmail.com>
2022-02-19 03:10:06 +0530

    Update theme name

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@
  * @package Betelgeuse
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if ( ! defined( '_BETELGEUSE_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define( '_BETELGEUSE_VERSION', '1.0.0' );
 }
 
 /**
@@ -138,10 +138,10 @@ add_action( 'widgets_init', 'betelgeuse_widgets_init' );
  * Enqueue scripts and styles.
  */
 function betelgeuse_scripts() {
-	wp_enqueue_style( 'betelgeuse-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'betelgeuse-style', get_stylesheet_uri(), array(), _BETELGEUSE_VERSION );
 	wp_style_add_data( 'betelgeuse-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'betelgeuse-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'betelgeuse-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _BETELGEUSE_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- inc/customizer.php
+++ inc/customizer.php
@@ -56,6 +56,6 @@ function betelgeuse_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function betelgeuse_customize_preview_js() {
-	wp_enqueue_script( 'betelgeuse-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'betelgeuse-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _BETELGEUSE_VERSION, true );
 }
 add_action( 'customize_preview_init', 'betelgeuse_customize_preview_js' );
